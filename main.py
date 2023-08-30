6ba9503dd3c7c408a9e9eac8fdaf1821382bcbf1
handowl <elisey.trushkov@gmail.com>
2023-08-31 01:33:16 +0700

    start added

--- main.py
+++ main.py
@@ -7,5 +7,7 @@ async def on_startup(_):
     print("Started")
 
 
+dp.register_message_handler(start_handler, commands=["start"])
+
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup)
